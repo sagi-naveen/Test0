2749ad833023e9ab7d35d35245f4c4a57f038f73
root <root@ip-172-31-22-92.ec2.internal>
2025-11-06 13:54:32 +0000

    a1

--- app.java
+++ app.java
@@ -1,6 +1,24 @@
+<<<<<<< Updated upstream
 .box {
   border: 2px solid #ccc;
   padding: 20px;
   margin: 15px;
   background-color: white;
 }
+=======
+.container {
+  display: flex;
+  justify-content: space-around; /* Distributes items evenly with space around them */
+  align-items: center; /* Vertically centers items */
+  height: 200px;
+  border: 1px solid blue;
+}
+
+.item {
+  width: 80px;
+  height: 80px;
+  background-color: lightblue;
+  text-align: center;
+  line-height: 80px;
+}
+>>>>>>> Stashed changes
